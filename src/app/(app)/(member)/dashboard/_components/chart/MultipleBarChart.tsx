ff86a0d1f96e3a899f7b3d54952acdae372f5dcc
jjminsuh <minsuh3203@gmail.com>
2025-06-16 10:52:53 +0900

    Feat: 일별 업무 평균 비교 그래프 데이터 api 연결

--- src/app/(app)/(member)/dashboard/_components/chart/MultipleBarChart.tsx
+++ src/app/(app)/(member)/dashboard/_components/chart/MultipleBarChart.tsx
@@ -22,48 +22,12 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { useState } from "react";
+import { httpInterface } from "@/lib/api/httpInterface";
+import { ChartData, DataItem } from "@/types/statisticsType";
+import { useEffect, useState } from "react";
+import { transformChartData } from "./_utils/TransformData";
 
 export const description = "A multiple bar chart";
-
-const allChartData = {
-  email: [
-    { day: "FRI", email: 10, avg: 6 },
-    { day: "SAT", email: 0, avg: 0 },
-    { day: "SUN", email: 0, avg: 0.3 },
-    { day: "MON", email: 12, avg: 20 },
-    { day: "TUE", email: 14, avg: 12 },
-    { day: "WED", email: 17, avg: 7 },
-    { day: "THU", email: 19, avg: 4 },
-  ],
-  git: [
-    { day: "FRI", git: 10, avg: 8 },
-    { day: "SAT", git: 0, avg: 0 },
-    { day: "SUN", git: 0, avg: 0 },
-    { day: "MON", git: 13, avg: 10 },
-    { day: "TUE", git: 8, avg: 9 },
-    { day: "WED", git: 6, avg: 7 },
-    { day: "THU", git: 9, avg: 8 },
-  ],
-  docs: [
-    { day: "FRI", docs: 3, avg: 2.5 },
-    { day: "SAT", docs: 0, avg: 0 },
-    { day: "SUN", docs: 0, avg: 0 },
-    { day: "MON", docs: 2, avg: 2 },
-    { day: "TUE", docs: 3, avg: 3 },
-    { day: "WED", docs: 4, avg: 3.5 },
-    { day: "THU", docs: 3, avg: 3 },
-  ],
-  teams: [
-    { day: "FRI", teams: 5, avg: 4 },
-    { day: "SAT", teams: 0, avg: 0 },
-    { day: "SUN", teams: 0, avg: 0 },
-    { day: "MON", teams: 6, avg: 5 },
-    { day: "TUE", teams: 9, avg: 7 },
-    { day: "WED", teams: 6, avg: 6 },
-    { day: "THU", teams: 10, avg: 8 },
-  ],
-};
 
 const chartConfig = {
   email: {
@@ -88,12 +52,32 @@ const chartConfig = {
   },
 } satisfies ChartConfig;
 
+const emptyChartData: ChartData = {
+  email: [],
+  git: [],
+  docs: [],
+  teams: [],
+};
+
 export function ChartBarMultiple() {
+  const [chartData, setChartData] = useState<ChartData>(emptyChartData);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const rawData = await httpInterface.getStaticUser<DataItem[]>();
+      const avgRawData = await httpInterface.getAvgStaticUser<DataItem[]>();
+      const transformed = transformChartData(rawData, avgRawData);
+      setChartData(transformed);
+    };
+
+    fetchData();
+  }, []);
+
   const [filter, setFilter] = useState<"email" | "git" | "docs" | "teams">(
     "email"
   );
 
-  const filteredData = allChartData[filter];
+  const filteredData = chartData[filter];
 
   return (
     <Card>
