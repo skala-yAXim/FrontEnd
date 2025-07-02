482577dcd58ef7ad0fca59f41de33e37bb9c17dd
kosssshhhh <kosukwhon@gmail.com>
2025-07-02 16:27:58 +0900

    🔧 Fix: 대시보드 헤더 제거, UserComment 컴포넌트 개선

--- src/app/(app)/(member)/dashboard/_components/UserComment.tsx
+++ src/app/(app)/(member)/dashboard/_components/UserComment.tsx
@@ -1,54 +1,45 @@
 import SplitText from "@/components/typography/SplitText";
-import { Card, CardHeader } from "@/components/ui/card";
 import { Skeleton } from "@/components/ui/skeleton";
 import { useGetCommentUser } from "@/hooks/useDashboardQueries";
 
 export default function UserComment() {
   const { data, isLoading, isError } = useGetCommentUser();
 
-  console.log(data);
-
   if (isLoading) {
     return (
-      <Card className='border-1 overflow-hidden shadow-none'>
-        <CardHeader>
-          <Skeleton className='h-8 w-full mb-2' />
-        </CardHeader>
-      </Card>
+      <div className='flex flex-col py-10 items-center justify-center'>
+        <Skeleton className='h-12 w-3/4 mb-2' />
+      </div>
     );
   }
 
   if (isError) {
     return (
-      <Card className='border-1 overflow-hidden shadow-none'>
-        <CardHeader>
-          <p className='text-2xl font-semibold text-center'>
-            데이터를 불러오는데 문제가 발생했습니다
-          </p>
-        </CardHeader>
-      </Card>
+      <div className='flex flex-col py-10 items-center justify-center'>
+        <p className='text-3xl font-semibold text-center text-destructive'>
+          데이터를 불러오는데 문제가 발생했습니다
+        </p>
+      </div>
     );
   }
 
   const comment = data ? data.comment : "아직 오늘의 한줄평이 없어요";
 
   return (
-    <Card className='border-1 overflow-hidden shadow-none w-full'>
-      <CardHeader>
-        <SplitText
-          text={comment}
-          className='text-2xl font-semibold'
-          delay={100}
-          duration={0.6}
-          ease='power3.out'
-          splitType='chars'
-          from={{ opacity: 0, y: 40 }}
-          to={{ opacity: 1, y: 0 }}
-          threshold={0.1}
-          rootMargin='-100px'
-          textAlign='center'
-        />
-      </CardHeader>
-    </Card>
+    <div className='flex flex-col py-10 items-center justify-center'>
+      <SplitText
+        text={comment}
+        className='text-3xl font-semibold'
+        delay={100}
+        duration={0.6}
+        ease='power3.out'
+        splitType='chars'
+        from={{ opacity: 0, y: 40 }}
+        to={{ opacity: 1, y: 0 }}
+        threshold={0.1}
+        rootMargin='-100px'
+        textAlign='center'
+      />
+    </div>
   );
 }
