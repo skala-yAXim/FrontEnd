217972cac313837a258f92614b51ff559e505271
yongjun-0903 <472dyd@gmail.com>
2025-07-06 15:22:41 +0900

    refactor: 대시보드 그래프 개선

--- src/app/(app)/(leader)/team-dashboard/_components/chart/MultipleBarChart.tsx
+++ src/app/(app)/(leader)/team-dashboard/_components/chart/MultipleBarChart.tsx
@@ -119,7 +119,7 @@ export function ChartBarMultiple() {
           className='w-full flex-1 flex flex-col items-center'
         >
           <div className='w-full h-full flex-1 px-2 md:px-4 py-0 mx-auto'>
-            <ResponsiveContainer width='100%' height={350}>
+            <ResponsiveContainer width='100%' height='100%'>
               <ComposedChart
                 layout='vertical'
                 accessibilityLayer
