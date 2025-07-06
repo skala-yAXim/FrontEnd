c2b2efacf86197518f5fa9c5295a113d5edb36bd
yongjun-0903 <472dyd@gmail.com>
2025-07-06 15:22:41 +0900

    refactor: 대시보드 그래프 개선

--- src/app/(app)/(member)/dashboard/_components/chart/StackedBarChart.tsx
+++ src/app/(app)/(member)/dashboard/_components/chart/StackedBarChart.tsx
@@ -104,7 +104,7 @@ export function ChartBarStacked() {
           className='w-full flex-1 flex flex-col items-center'
         >
           <div className='w-full h-full flex-1 px-2 md:px-4 py-0 mx-auto'>
-            <ResponsiveContainer width='100%' height={350}>
+            <ResponsiveContainer width='100%' height='100%'>
               <BarChart accessibilityLayer data={chartData}>
                 <CartesianGrid
                   vertical={false}
